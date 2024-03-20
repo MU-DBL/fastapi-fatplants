27de709f221d839628ceb1723ecf8ad94c94c101
Yongfang Qin <yongfang012@gmail.com>
2024-03-20 08:56:31 -0500

    Revert "Chitti"

--- app_fatplants/router_imports.py
+++ app_fatplants/router_imports.py
@@ -1,11 +1,9 @@
 from species import species
 from kegg_pathway import kegg
 from chatgpt import chat
-from contactus import send_email
 
 routers=[
     species,
     kegg,
-    chat,
-    send_email
+    chat
 ]
