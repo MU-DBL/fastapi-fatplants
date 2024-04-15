f3e44532b56d814d4f79077839708c21c456a4e9
gk94c <gk94c@ip-172-31-0-253.ec2.internal>
2024-04-15 05:08:46 +0000

    security incident fix - cross-origin

--- app_fatplants/main.py
+++ app_fatplants/main.py
@@ -28,7 +28,7 @@ async def shutdown():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://fatplants.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
