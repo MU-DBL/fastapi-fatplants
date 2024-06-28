e402260bdb11739e2829cddc24a8c3dfae993b78
gk94c <gk94c@ip-172-31-0-253.ec2.internal>
2024-06-28 20:19:06 +0000

    godata api + new fatplants volume

--- app_fatplants/router_imports.py
+++ app_fatplants/router_imports.py
@@ -1,9 +1,11 @@
 from species import species
 from kegg_pathway import kegg
 from chatgpt import chat
+from networks import goenrichment
 
 routers=[
     species,
     kegg,
-    chat
+    chat,
+    goenrichment
 ]
