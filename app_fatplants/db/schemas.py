9b79c3ba13a00e34dad5b4b7ef7cf1b4d8920404
gk94c_govardhan <khadakkargovardhan@gmail.com>
2024-09-06 15:09:44 -0500

    aralip form submit and fetch api - commented

--- app_fatplants/db/schemas.py
+++ app_fatplants/db/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class lmpd_index(BaseModel):
     identifier:str
@@ -56,3 +57,31 @@ class soya_identifier(BaseModel):
 
     # class Config:
     #     orm_mode = True
+
+class ArabidopsisRecord(BaseModel):
+    name: str
+    affiliation: str
+    email_address: str
+    record_type: str
+    record_url: Optional[str] = None
+    figure_url: Optional[str] = None
+    pathway: Optional[str] = None
+    protein_family_name_common: Optional[str] = None
+    protein_family_abbreviation_common: Optional[str] = None
+    isoform_gene_name: Optional[str] = None
+    isoform_gene_abbreviation: Optional[str] = None
+    gene_locus: Optional[str] = None
+    ec_number: Optional[str] = None
+    subcellular_location_listed: Optional[str] = None
+    subcellular_location_filled: Optional[str] = None
+    mutant_name: Optional[str] = None
+    evidence_for_function_listed: Optional[str] = None
+    evidence_for_function_filled: Optional[str] = None
+    specific_comments_on_locus: Optional[str] = None
+    other_comments: Optional[str] = None
+    reference_1: Optional[str] = None
+    pubmed_link_1: Optional[str] = None
+    reference_2: Optional[str] = None
+    pubmed_link_2: Optional[str] = None
+    reference_3: Optional[str] = None
+    pubmed_link_3: Optional[str] = None
--- app_fatplants/router_imports.py
+++ app_fatplants/router_imports.py
@@ -3,11 +3,13 @@ from kegg_pathway import kegg
 from chatgpt import chat
 from networks import goenrichment
 from contact_us import send_email
+from aralipform import form
 
 routers=[
     species,
     kegg,
     chat,
     goenrichment,
-    send_email
+    send_email,
+    form
 ]
