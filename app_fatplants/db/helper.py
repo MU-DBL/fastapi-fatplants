131fd5d71517cfdad937a0973b9a576a207c18da
gcycaas <gcycaas@gmail.com>
2024-10-21 20:50:21 -0500

    Update helper.py
    Allow some characters for searching

--- app_fatplants/db/helper.py
+++ app_fatplants/db/helper.py
@@ -1,9 +1,12 @@
 import re
 
-def is_sql_injection(input_value):
+def is_sql_injection(input_value, search=False):
     # Regular expression pattern to detect common SQL injection characters or patterns
     # edit if any of the symbol is required in input
-    sql_injection_pattern = r'[;\'"()\|{}=+*[\]<>?`~&^%$#@!]'
+    if search:
+        sql_injection_pattern = r'[\'"\|{}=+*[\]<>?`~^%$#@!]' #Allow some characters for searching
+    else:
+        sql_injection_pattern = r'[;\'"()\|{}=+*[\]<>?`~&^%$#@!]'
     if re.search(sql_injection_pattern, input_value):
         return True  # SQL injection detected
     else:
